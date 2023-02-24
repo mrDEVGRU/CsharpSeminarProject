6c4ed9016276addca9783097e7f30e7d6578e034
mrDEVGRU <mrdevgru@gmail.com>
2023-02-24 18:39:33 +0400

    Сделан код к Homework_01_Ex3

--- Homework/Homework_01/Ex3/Program.cs
+++ Homework/Homework_01/Ex3/Program.cs
@@ -1,1 +1,7 @@
 ﻿
+Console.WriteLine("Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным.");
+
+Console.Write("Введите число = ");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0) Console.Write("Yes");
+else Console.Write("No");
