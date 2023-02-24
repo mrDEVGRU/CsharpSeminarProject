b9c9ccb443a34d420c3435291b4d9e3aaa0a9cae
mrDEVGRU <mrdevgru@gmail.com>
2023-02-24 17:27:04 +0400

    Сделана блок-схема к Homework_01 Ex1

---
+++ Homework/Homework_01/Ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.");
+
+Console.Write("Введите number1 = ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите number2 = ");
+int number2 = int.Parse(Console.ReadLine());
+int max = number1;
+int min = number2;
+if (number1 > number2)
+{
+}
+else
+{
+    min = number1;
+    max = number2;
+}
+Console.WriteLine(min);
+Console.WriteLine(max);
