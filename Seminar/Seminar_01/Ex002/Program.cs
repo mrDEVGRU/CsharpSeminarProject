53c6e0cec74e2ab8e7c45bad13dc2e28af27b8d4
mrDEVGRU <mrdevgru@gmail.com>
2023-02-24 15:49:22 +0400

    Написан код

--- Seminar/Seminar_01/Ex002/Program.cs
+++ Seminar/Seminar_01/Ex002/Program.cs
@@ -4,3 +4,10 @@ Console.WriteLine("Показать чётные числа от 1 до N");
 int a = 1;
 Console.Write("Введите число N ");
 int m = int.Parse(Console.ReadLine());
+for (int i = a; i <= m; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
