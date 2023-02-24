1ab7cec624931936276524c90a84160efaecaa1d
mrDEVGRU <mrdevgru@gmail.com>
2023-02-24 18:45:33 +0400

    Сделан код к Homework_01_Ex4

--- Homework/Homework_01/Ex4/Program.cs
+++ Homework/Homework_01/Ex4/Program.cs
@@ -1,1 +1,13 @@
 ﻿
+Console.WriteLine("Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.");
+
+int a = 1;
+Console.Write("Введите число N ");
+int m = int.Parse(Console.ReadLine());
+for (int i = a; i <= m; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
