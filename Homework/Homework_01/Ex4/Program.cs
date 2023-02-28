60defe5fc6ee77b30431997a724b49949daed0d8
mrDEVGRU <mrdevgru@gmail.com>
2023-02-28 14:09:32 +0400

    Исправил Ex4

--- Homework/Homework_01/Ex4/Program.cs
+++ Homework/Homework_01/Ex4/Program.cs
@@ -1,10 +1,9 @@
 ﻿
 Console.WriteLine("Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.");
 
-int a = 1;
 Console.Write("Введите число N ");
 int m = int.Parse(Console.ReadLine());
-for (int i = a; i <= m; i++)
+for (int i = 1; i <= m; i++)
 {
     if (i % 2 == 0)
     {
