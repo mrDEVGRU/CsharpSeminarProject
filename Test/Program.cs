a4f9e66c93c1ec17a499736532013b795985f570
mrDEVGRU <mrdevgru@gmail.com>
2023-03-01 13:06:35 +0400

    test

---
+++ Test/Program.cs
@@ -0,0 +1,23 @@
+﻿// test proverka
+
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string Getnumbers(int count)
+{
+    string output = String.Empty;
+    int index = 1;
+
+    while (index <= count)
+    {
+        if (index % 2 == 0)
+            if (index >= count - 1) output = output + index;
+            else
+                output = output + index + ", ";
+        index++;
+    }
+    return output;
+}
+
+string res = Getnumbers(N);
+Console.WriteLine(res);
