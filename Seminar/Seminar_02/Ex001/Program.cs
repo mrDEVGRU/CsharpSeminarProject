73737bb858df3e22a1b63889b4b9c1b6f2abdfd1
mrDEVGRU <mrdevgru@gmail.com>
2023-03-01 12:10:27 +0400

    start

---
+++ Seminar/Seminar_02/Ex001/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Удалить вторую цифру трёхзначного числа");
+
+int Number = new Random().Next(100, 1000);
+int NewNumber = 0;
+
+NewNumber = Number / 100 * 10 + Number % 10;
+
+Console.WriteLine($"Если из числа {Number} удалить вторую цифру, получится число {NewNumber}");
---
+++ Seminar/Seminar_02/Ex002/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Остаток от двух чисел");
+
+Console.Write("Введите number1 = ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите number2 = ");
+int number2 = int.Parse(Console.ReadLine());
+int result = 0;
+
+int MultipleNumber()
+{
+    result = number1 % number2;
+    return result;
+}
+
+MultipleNumber();
+
+Console.WriteLine();
+if (result == 0) Console.WriteLine($"Число {number1} кратно числу {number2}");
+else
+{
+    Console.WriteLine($"Число {number1} не кратно числу {number2}, остаток от деления равен {number1 % number2}");
+}
+Console.WriteLine();
