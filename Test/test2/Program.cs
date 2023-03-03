35f79e3491210f10921d3f0eaff05d3cbeefcdee
mrDEVGRU <mrdevgru@gmail.com>
2023-03-03 15:10:22 +0400

    test3

--- Test/test2/Program.cs
+++ Test/test2/Program.cs
@@ -1,14 +1,14 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-int Prompt(string message)
+int Prompt(string message)                          // функция
 {
-    Console.Write(message);
+    System.Console.Write(message);
     string value = Console.ReadLine();
     int result = Convert.ToInt32(value);
     return result;
 }
 
-int number = Prompt("Введите трехзначное число > ");
+int number = Prompt("Введите трехзначное число > ");            // применение функции
 if (number < 100 || number >= 1000)
 {
     Console.WriteLine("Вы ввели не  трехзначное число, пожалуйста повторите ввод");
---
+++ Test/test3/Program.cs
@@ -0,0 +1,34 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int Prompt(string message)                          // функция
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int GetThirdRank(int number)                // функция 2
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+bool ValidateNumber(int number)               // функция 3
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+
+int number = Prompt("Введите число > ");
+if (ValidateNumber(number))
+{
+    Console.WriteLine(GetThirdRank(number));
+}
