cb304a89d3b722110fb5762ddf246551f0215f7c
mrDEVGRU <mrdevgru@gmail.com>
2023-03-03 15:54:51 +0400

    Seminar_02_Ex004

---
+++ Seminar/Seminar_02/Ex004/Program.cs
@@ -0,0 +1,15 @@
+﻿// 1. По двум введённым числам проверять является ли первое квадратом второго 
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+string txtResult = $"Число {number1} не является квадратом числа {number2}";    //вывод результата проверки на экран
+
+if (number1 == number2 * number2)
+{
+    txtResult = txtResult.Replace(" не", "");  //если условие выполняется, "не" заменяется пробелом
+}
+Console.WriteLine(txtResult);
