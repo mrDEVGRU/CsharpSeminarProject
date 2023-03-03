ab743550bd208e04d8c3f57a6dc24516904964c9
mrDEVGRU <mrdevgru@gmail.com>
2023-03-03 15:53:32 +0400

    Seminar_02_Ex003

---
+++ Seminar/Seminar_02/Ex003/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет,  кратно ли оно  одновременно 7 и 23.
+
+int Prompt(string message)                          // функция
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int number = Prompt("Введите число > ");
+
+if (number % 7 == 0 && number % 23 == 0)
+    Console.WriteLine($"Число {number} кратно 7 и 23");
+else Console.WriteLine($"Число {number} не кратно одновременно 7 и 23");
