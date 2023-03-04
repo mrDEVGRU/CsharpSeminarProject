aaa004ab8f37e3d0dadddcfce19e0235d1f6c954
mrDEVGRU <mrdevgru@gmail.com>
2023-03-04 12:07:39 +0400

    Intern_11

---
+++ Tasks/intern_11/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа.
+
+int Number = new Random().Next(10, 100);
+int Max = 0;
+
+int MaxDigit()          // функция
+{
+    int DigitOne = Number / 10;         // int целое число десятки уберутся
+    int DigitTwo = Number % 10;         // % останется остаток они же десятки
+    if (DigitOne > DigitTwo) Max = DigitOne;
+    else Max = DigitTwo;
+
+    return Max;             // возврат для функции
+}
+MaxDigit();         // обрашение к функции
+Console.WriteLine($"В числе {Number} наибольшая цифра {Max}");
