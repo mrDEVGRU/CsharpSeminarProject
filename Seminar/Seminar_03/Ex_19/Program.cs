a113c90bb3be86ced3047e5175e802573df17e75
mrDEVGRU <mrdevgru@gmail.com>
2023-03-09 23:54:55 +0400

    Seminar_03_Ex_19

---
+++ Seminar/Seminar_03/Ex_19/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите метод(-ы), который принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите целое число: ");
+string number = Console.ReadLine();
+int length = number.Length;
+
+bool Palindrome()
+{
+    for (int i = 0; i < length / 2; i++)
+        while (number[i] == number[length - 1])
+        {
+            return true;
+        }
+    return false;
+}
+
+
+if (Palindrome()) Console.WriteLine($"Число {number} является палиндромом");
+else Console.WriteLine($"Число {number} не является палиндромом");
