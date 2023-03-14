2dda563e02634904c2446a386b4685e8c2a49b2e
mrDEVGRU <mrdevgru@gmail.com>
2023-03-14 18:06:53 +0400

    Seminar_04_Ex_25

---
+++ Seminar/Seminar_04/Ex_25/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит числа A в натуральную степень B.
+
+Console.WriteLine("Введите число: ");
+int value = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень: ");
+int index = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+for (int power = 0; power <= index; power++)
+    Console.WriteLine($"{value}^{power} = {Math.Pow(value, power):N0}");
