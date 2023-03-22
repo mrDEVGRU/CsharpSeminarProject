b5789450e2ed2847ea6943954d0c0da5e997c199
mrDEVGRU <mrdevgru@gmail.com>
2023-03-22 12:22:32 +0400

    Ex_34

--- Seminar/Seminar_05/Ex_34/Program.cs
+++ Seminar/Seminar_05/Ex_34/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int size = 10;
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintResult(int[] array)
+{
+    int even = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+        if (array[i] % 2 == 0) even++;
+    }
+    Console.WriteLine($"Количество четных чисел {even}");
+}
+
+PrintResult(FillArray(size));
