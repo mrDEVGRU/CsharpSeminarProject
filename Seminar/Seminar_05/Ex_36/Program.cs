3dcbc6566751a7403df7e676b915b519f7fdda3d
mrDEVGRU <mrdevgru@gmail.com>
2023-03-22 12:56:10 +0400

    Ex_36

--- Seminar/Seminar_05/Ex_36/Program.cs
+++ Seminar/Seminar_05/Ex_36/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int size = 10;
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1, 1000);
+    }
+    return array;
+}
+
+void PrintResult(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+        if (i % 2 == 1) sum = sum + array[i];
+    }
+    Console.WriteLine($"Сумма элементов нечётной позиции = {sum}");
+}
+
+PrintResult(FillArray(size));
