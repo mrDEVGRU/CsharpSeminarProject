706f3be68cef062081408643b5b900f62dace07f
mrDEVGRU <mrdevgru@gmail.com>
2023-03-23 16:44:26 +0400

    Ex_47

--- Seminar/Seminar_07/Ex_47/Program.cs
+++ Seminar/Seminar_07/Ex_47/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int[,] array = new int[3, 4];
+Random rand = new Random();
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        array[i, j] = rand.Next(-10, 10);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
