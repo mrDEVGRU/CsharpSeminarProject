ec2e8ddde088cd527d6472afe7379f28e0484ead
mrDEVGRU <mrdevgru@gmail.com>
2023-03-28 20:08:29 +0400

    Fix array.GetLength()

--- Seminar/Seminar_07/Ex_47/Program.cs
+++ Seminar/Seminar_07/Ex_47/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
 int[,] array = new int[3, 4];
-for (int i = 0; i < 3; i++)
+for (int i = 0; i < array.GetLength(0); i++)
 {
-    for (int j = 0; j < 4; j++)
+    for (int j = 0; j < array.GetLength(1); j++)
     {
         array[i, j] = new Random().Next(-10, 10);
         Console.Write(array[i, j] + " ");
--- Seminar/Seminar_07/Ex_50/Program.cs
+++ Seminar/Seminar_07/Ex_50/Program.cs
@@ -3,9 +3,9 @@
 
 
 int[,] array = new int[3, 4];
-for (int i = 0; i < 3; i++)
+for (int i = 0; i < array.GetLength(0); i++)
 {
-    for (int j = 0; j < 4; j++)
+    for (int j = 0; j < array.GetLength(1); j++)
     {
         array[i, j] = new Random().Next(1, 10);
         Console.Write(array[i, j] + " ");
