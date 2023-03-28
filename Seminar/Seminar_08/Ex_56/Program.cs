2f6876276fb3e6bc64562c0861db7ae93a011c0a
mrDEVGRU <mrdevgru@gmail.com>
2023-03-28 17:42:35 +0400

    Ex_56

--- Seminar/Seminar_08/Ex_56/Program.cs
+++ Seminar/Seminar_08/Ex_56/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int[,] GetMatrix()
+{
+    int[,] array = new int[3, 4];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 6);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int rowNumber(int[,] array)
+{
+    int index = 0;
+    int sum = 0;
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (i == 0) result = sum;
+        else if (sum < result)
+        {
+            result = sum;
+            index = i;
+        }
+    }
+    return index;
+}
+
+int[,] matrix = GetMatrix();
+PrintMatrix(matrix);
+Console.WriteLine("---");
+int index = rowNumber(matrix);
+Console.WriteLine($"{index + 1} номер строки с наименьшей суммой элементов");
