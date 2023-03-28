6be3ac2ebd6604ee99904d87597a2fffee58c20f
mrDEVGRU <mrdevgru@gmail.com>
2023-03-28 14:46:46 +0400

    Seminar_07

--- Seminar/Seminar_07/Ex_50/Program.cs
+++ Seminar/Seminar_07/Ex_50/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+
+int[,] array = new int[3, 4];
+Random rand = new Random();
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        array[i, j] = rand.Next(1, 10);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine($"Элемент на позиции 3, 4 > {array[2, 3]}");
--- Seminar/Seminar_07/Ex_52/Program.cs
+++ Seminar/Seminar_07/Ex_52/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] GetMatrix()
+{
+    int[,] array = new int[3, 4];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] >= 10) Console.Write($" {array[i, j]}  ");
+            else Console.Write($"  {array[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] GetArithmeticMean(int[,] array)
+{
+    double[] result = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+            result[j] = sum / array.GetLength(0);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[] array)
+{
+    Console.WriteLine("-------------------");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i].ToString("F" + 1)}  ");
+    }
+}
+
+int[,] matrix = GetMatrix();
+PrintMatrix(matrix);
+double[] arithmeticMean = GetArithmeticMean(matrix);
+PrintArray(arithmeticMean);
