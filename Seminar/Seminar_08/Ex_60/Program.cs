2878740b73ccdd30b5d51ab8f13f33b0e7c94b05
mrDEVGRU <mrdevgru@gmail.com>
2023-03-28 19:07:25 +0400

    Ex_60

--- Seminar/Seminar_08/Ex_60/Program.cs
+++ Seminar/Seminar_08/Ex_60/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+
+void PrintArray(int[,,] array3D)
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                Console.Write($"{array3D[j, k, i]}({j},{k},{i}); ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+void CreateArray(int[,,] array3D)
+{
+    int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+    int number;
+    for (int i = 0; i < temp.GetLength(0); i++)
+    {
+        temp[i] = new Random().Next(1, 10);
+        number = temp[i];
+        for (int j = 0; j < i; j++)
+        {
+            while (temp[i] == temp[j])
+            {
+                temp[i] = new Random().Next(1, 10);
+                j = 0;
+            }
+        }
+    }
+    int count = 0;
+    for (int x = 0; x < array3D.GetLength(0); x++)
+    {
+        for (int y = 0; y < array3D.GetLength(1); y++)
+        {
+            for (int z = 0; z < array3D.GetLength(2); z++)
+            {
+                array3D[x, y, z] = temp[count];
+                count++;
+            }
+        }
+    }
+}
+
+int[,,] array3D = new int[2, 2, 2];
+CreateArray(array3D);
+PrintArray(array3D);
