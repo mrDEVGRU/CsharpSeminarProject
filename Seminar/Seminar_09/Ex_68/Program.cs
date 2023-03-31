6e83a34f8aab22d965fd5822e1ab19fae52b0392
mrDEVGRU <mrdevgru@gmail.com>
2023-03-31 23:32:42 +0400

    Ex_68

--- Seminar/Seminar_09/Ex_68/Program.cs
+++ Seminar/Seminar_09/Ex_68/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n
+
+
+int akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return akkerman(m - 1, 1);
+    else return akkerman(m - 1, akkerman(m, n - 1));
+}
+
+
+int m = 2;
+int n = 3;
+Console.Write($"Функция Аккерамана = {akkerman(m, n)}");
