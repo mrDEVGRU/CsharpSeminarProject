0a4ce844239b25637da8798ed243fdbec645a0b5
mrDEVGRU <mrdevgru@gmail.com>
2023-03-31 22:53:33 +0400

    Ex_64

--- Seminar/Seminar_09/Ex_64/Program.cs
+++ Seminar/Seminar_09/Ex_64/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+int enumNumber(int N)
+{
+    if (N < 1)
+    {
+        return 1;
+    }
+    Console.Write(" {0},", N);
+    N--;
+    return enumNumber(N);
+}
+
+int N = 20;
+enumNumber(N);
