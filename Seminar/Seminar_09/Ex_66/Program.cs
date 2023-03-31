7269e223932743c43ca25bbfa4fd62f333cea7e1
mrDEVGRU <mrdevgru@gmail.com>
2023-03-31 23:17:40 +0400

    Ex_66

--- Seminar/Seminar_09/Ex_66/Program.cs
+++ Seminar/Seminar_09/Ex_66/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int sumNumber(int M, int N)
+{
+    if (M == N) return N;
+    else return M + sumNumber(M + 1, N);
+}
+
+int M = 1;
+int N = 6;
+Console.Write($"Сумма натуральных элементов в промежутке от {M} до {N} = {sumNumber(M, N)}");
